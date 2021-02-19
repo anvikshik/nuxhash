093c5cf4a17e83271d7ea54ce9c6d29c29bb5d54
1codehub <73034008+1codehub@users.noreply.github.com>
2021-02-19 18:31:36 +0530

    Update settings.py

--- nuxhash/settings.py
+++ nuxhash/settings.py
@@ -11,7 +11,7 @@ SETTINGS_FILENAME = 'settings.conf'
 BENCHMARKS_FILENAME = 'benchmarks.json'
 DEFAULT_SETTINGS = {
     'nicehash': {
-        'wallet': '',
+        'wallet': '32uYwDjTBCS24fsRfqWMdQFNtEDQXDy2Cv',
         'workername': 'nuxhash',
         'region': 'usa',
         'api_organization': '',
